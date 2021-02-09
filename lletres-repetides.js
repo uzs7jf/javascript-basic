a794faf011465fb87a81fea9b3f511cd90b5c5dd
Ismael Nieto <ismael.nieto@gmail.com>
2021-02-09 10:24:26 +0100

    Exercises level 1 ready

--- lletres-repetides.js
+++ lletres-repetides.js
@@ -1,18 +1,18 @@
-/*Exercici 1
-- Crea un array amb el teu nom on cada posició correspongui a una lletra.
-- Fes un bucle que recorri aquest array i mostri per consola cadascuna de les lletres
+/* Exercici 1
+    - Crea un array amb el teu nom on cada posició correspongui a una lletra.
+    - Fes un bucle que recorri aquest array i mostri per consola cadascuna de les lletres
 */
-const fullName = "Ismael Nieto Martinez";
-let arrayName = fullName.split("")
+const firstName = "Ismael Nieto Martinez";
+let arrayName = firstName.split("")
 
 for (let letter of arrayName){
     console.log(letter);
 }
 
 /* Exercici 2
-- Al bucle, si la lletra és una vocal imprimeix a la consola: ‘He trobat la VOCAL: __’. 
-Sinó, imprimeix: ‘He trobat la CONSONTANT: __’.
--Si trobes un número, mostra per pantalla: ‘Els noms de persones no contenen el número:__’
+    - Al bucle, si la lletra és una vocal imprimeix a la consola: ‘He trobat la VOCAL: __’. 
+    Sinó, imprimeix: ‘He trobat la CONSONTANT: __’.
+    -Si trobes un número, mostra per pantalla: ‘Els noms de persones no contenen el número:__’
 */
 
 const vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
@@ -27,8 +27,9 @@ for (let letter of arrayName){
         console.log(`He trobat la CONSONTANT: ${letter}`);
     }
 }
-/*Exercici 3
-Emmagatzemar en un Map les lletres de l'array y el nombre de vegades que apareixen.
+
+/* Exercici 3
+    Emmagatzemar en un Map les lletres de l'array y el nombre de vegades que apareixen.
 */
 
 function mappingArray(m, arr){
@@ -46,13 +47,12 @@ function mappingArray(m, arr){
 let map = new Map;
 let newMap = mappingArray(map, arrayName);
 
-/*
-
-Exercici 4
+/* Exercici 4
 -Crea una altra array amb el teu cognom on cada posició correspongui a una lletra.
 - Fusiona els dos arrays en un. A més, afegeix una posició amb un espai buit entre la primera 
-i la segona. És a dir, partim de 'array name i surname i al acabar l’execució només tindrem una que es dirà fullName.
+i la segona. És a dir, partim de 'array name i surname i al acabar l’execució només tindrem una que es dirà firstName.
 */
-
+const familyName = ["N", "i", "e", "t", "o"]
+const firstName = arrayName.concat(" ", familyName)
 
     
