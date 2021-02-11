1b74e6419e7a3d71b4635459dc26546d1bc0fe77
Ismael Nieto <ismael.nieto@gmail.com>
2021-02-11 12:12:31 +0100

    variableName added

--- functions.js
+++ functions.js
@@ -49,7 +49,8 @@ for (let i = 0; i<= numFunctions; i++) {
 - Exercici 2
 Crear una funció anònima autoinvocable (igualada a una variable) que imprimeixi per pantalla el nom d'usuari rebut com a paràmetre
 */
-
-(function UserName(user){
+const newVariable = (function UserName(user){
     console.log(user)
 })(prompt("Introduzca su nombre: "))   
+        
+    
