f8c10f0868b8d4cb8d27887885b9c7245b243e80
Ismael Nieto <ismael.nieto@gmail.com>
2021-02-18 13:58:30 +0100

    1 exercise Nivell 2: done!

--- promises_callbacks.js
+++ promises_callbacks.js
@@ -9,7 +9,7 @@ o bé reject() que rep. Invocar-la des de fora pasandole totes dues funcions que
 const promise = () => {
     return new Promise((resolve, reject) => {
         const randomNumber = Math.floor(Math.random() * 11); // Return interger between 0 and 5
-        if(randomNumber > 5){
+        if (randomNumber > 5) {
             resolve();
         } else {
             reject();
@@ -17,18 +17,67 @@ const promise = () => {
     });
 };
 
-promise().then(()=> console.log("You pass the exercise."))
-         .catch(()=> console.log("Ups, you should try the exercise again"))
+promise().then(() => console.log("You pass the exercise."))
+    .catch(() => console.log("Ups, you should try the exercise again"))
 /* - Exercici 2
 Crear una arrow function que, rebent un paràmetre i una function callback, 
 li passi a la funció un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre.
  */
 
-const myCallBack = function(n){
-    total = (n  + 4 );
+const myCallBack = function (n) {
+    total = (n + 4);
     console.log(`el resultado es ${total}`);
 }
 
-const myArrowFunction = (x, callback)=> callback(x)
+const myArrowFunction = (x, callback) => callback(x)
 
 myArrowFunction(11, myCallBack);
+
+/*Nivell 2
+- Exercici 1
+Donats els objectes employees i salaries, creu una arrow function getEmpleado 
+que retorni una Promise efectuant la cerca en l'objecte pel seu id.
+
+let employees = [
+    {id: 1, name: 'Linux Torvalds'}, 
+    {id: 2, name: 'Bill Gates'},
+    {id: 3,name: 'Jeff Bezos'}
+];
+let salaries = [
+    {id: 1, salary: 4000},
+    {id: 2, salary: 1000},
+    {id: 3,  salary: 2000}
+];
+ */
+
+let employees = [
+    { id: 1, name: 'Linux Torvalds' },
+    { id: 2, name: 'Bill Gates' },
+    { id: 3, name: 'Jeff Bezos' }
+];
+
+const numberID = 5;
+
+const promiseEmpleado = () => {
+    return new Promise((resolve, reject) => {
+        let isEmpleado = employees.find(x => x.id === numberID)
+        if(isEmpleado){
+            resolve();
+        } else {
+            reject();
+        }
+    });
+}
+
+
+promiseEmpleado()
+        .then(() => console.log("Employee found"))
+        .catch(() => console.log("Ups, no employee here!"))
+
+
+/*Exercici 2
+Creu una altra arrow function getSalario que rebi com a paràmetre un objecte employee i retorni el seu salari.
+
+ */
+
+
