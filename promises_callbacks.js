374dcadb2b9110cc8078de524e9bc85719c91099
Ismael Nieto <ismael.nieto@gmail.com>
2021-02-19 13:52:13 +0100

    Nivel 2.2 exercise almost ready

--- promises_callbacks.js
+++ promises_callbacks.js
@@ -17,8 +17,10 @@ const promise = () => {
     });
 };
 
-promise().then(() => console.log("You pass the exercise."))
+promise()
+    .then(() => console.log("You pass the exercise."))
     .catch(() => console.log("Ups, you should try the exercise again"))
+
 /* - Exercici 2
 Crear una arrow function que, rebent un paràmetre i una function callback, 
 li passi a la funció un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre.
@@ -38,16 +40,6 @@ myArrowFunction(11, myCallBack);
 Donats els objectes employees i salaries, creu una arrow function getEmpleado 
 que retorni una Promise efectuant la cerca en l'objecte pel seu id.
 
-let employees = [
-    {id: 1, name: 'Linux Torvalds'}, 
-    {id: 2, name: 'Bill Gates'},
-    {id: 3,name: 'Jeff Bezos'}
-];
-let salaries = [
-    {id: 1, salary: 4000},
-    {id: 2, salary: 1000},
-    {id: 3,  salary: 2000}
-];
  */
 
 let employees = [
@@ -56,28 +48,49 @@ let employees = [
     { id: 3, name: 'Jeff Bezos' }
 ];
 
-const numberID = 5;
-
-const promiseEmpleado = () => {
+const getEmpleado = (numberID) => {
     return new Promise((resolve, reject) => {
         let isEmpleado = employees.find(x => x.id === numberID)
-        if(isEmpleado){
-            resolve();
+        if (isEmpleado) {
+            resolve(isEmpleado);
         } else {
             reject();
         }
     });
 }
 
-
-promiseEmpleado()
-        .then(() => console.log("Employee found"))
-        .catch(() => console.log("Ups, no employee here!"))
-
+/* let empleadoID = 2
+getEmpleado(empleadoID)
+    //.then((val) => console.log(`Employee found: ${JSON.stringify(val)}`))
+    .then((val) => val)
+    .catch(() => console.log("Ups, no employee here!")) */
 
 /*Exercici 2
 Creu una altra arrow function getSalario que rebi com a paràmetre un objecte employee i retorni el seu salari.
+*/
 
- */
+let salaries = [
+    { id: 1, salary: 4000 },
+    { id: 2, salary: 1000 },
+    { id: 3, salary: 2000 }
+];
 
+
+const getSalario = (obj) => {
+
+    return new Promise((resolve, reject) => {
+        let isSalary = salaries.find(x => x.id === obj.id);
+        //console.log("salri", isSalary.salary);
+        if (isSalary) {
+            revolve(isSalary.salary);
+        } else {
+            reject();
+        } 
+    });
+
+}
+    
+getSalario(employees[1])
+    .then((salary) => console.log(salary))
+    .catch(() => console.log("Ups, no salary found!!!"))
 
